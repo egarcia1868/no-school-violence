af3f22c5b1929bce051f791eac68f133e17dbfd9
MelanyNadine <55468076+MelanyNadine@users.noreply.github.com>
2021-02-10 23:43:48 -0500

    Update App.js
    This is the sketch of the main idea. If any improvements come to your mind, please modify the code, have a nice week!

--- src/App.js
+++ src/App.js
@@ -3,22 +3,37 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main className="App">
+
+{/* ********************************** */
+/*            HEADER                  */
+/* ********************************** */}
+
+      <header className="header">
+        <div class="logo_nsv">
+          <img src={logo} width="80px"/>
+        </div>
+        <div class="Donate">
+          <button>Donate</button>
+        </div>
       </header>
-    </div>
+
+{/************************************/
+/*              FORM                */
+/************************************/}
+
+      <section className="mainframe">
+        <h1>Welcome to Lantern</h1>
+        <p>Have you seen behavioural changes in your child and you want to know if such behaviours could
+            trigger any type of violence? <br/><br/>
+            Enter the words you think may fit with your child's behaviour:
+        </p>
+
+        <form action="">
+          <input type="text" id="keywords" name="key_vals" placeholder="angry, boredom, violent"/>
+        </form>
+      </section>
+    </main>
   );
 }
 
