1d0cda9a90a7f3d913ee3b394bce884971b4a6ab
Eric Garcia <egarcia1868@gmail.com>
2021-02-13 18:15:47 -0600

    created the autosuggest behavior add bar, also adding a simple program for unjumbling the list of behavoirs

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import SearchBar from "../components/SearchBar";
 
 const Form = (props) => {
+  const [search, setSearch] = useState("");
+  const [selectedBehaviors, setSelectedBehaviors] = useState([]);
+
+  const handleInputChange = event => {
+    const value = event.target.value;
+    setSearch( value )
+  }
+
+  const handleSearchSubmit = event => {
+    event.preventDefault();
+    //at this point I will need to move the entered behavior into the box on the right
+    
+  }
+
+  const handleFormSubmit = event => {
+    event.preventDefault();
+    //once the backend is live we'll need the code here to submit our selected behaviors
+  }
 
   return (
     <section className="mainframe">
@@ -10,6 +29,12 @@ const Form = (props) => {
             trigger any type of violence? <br/><br/>
             Enter the words you think may fit with your child's behaviour:
         </p>
+        <SearchBar 
+          handleInputChange={handleInputChange}
+          value={search}
+          handleSearchSubmit={handleSearchSubmit}
+
+        />
 
       <form action="">
         <input type="text" id="keywords" name="key_vals" placeholder="angry, boredom, violent"/>
